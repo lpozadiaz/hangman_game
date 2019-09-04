6ec9562c816ae6c501f18f68760868d517e77150
Lorena Poza Díaz <lorenapozadiaz@MacLPD.local>
2019-09-04 14:00:47 +0200

    Third commit: time included

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ window.onload = function () {
   
   let wordsArr = ["everton", "liverpool", "swansea", "chelsea", "hull", "manchester-city", "newcastle-united", "alien", "dirty-harry", "gladiator", "finding-nemo", "jaws","manchester", "milan", "madrid", "amsterdam", "prague"];
   let hints = ["Based in Mersyside", "Based in Mersyside", "First Welsh team to reach the Premier Leauge", "Owned by A russian Billionaire", "Once managed by Phil Brown", "2013 FA Cup runners up", "Gazza's first club", "Science-Fiction horror film", "1971 American action film", "Historical drama", "Anamated Fish", "Giant great white shark", "Northern city in the UK", "Home of AC and Inter", "Spanish capital", "Netherlands capital", "Czech Republic capital"];
-  let drawArray = [rightLeg, leftLeg, rightArm, leftArm, torso, head, smallVerticalFrame, upperHorzontalFrame, verticalFrame, lowerFrame];
+  let drawArray = [rightLeg, leftLeg, rightArm, leftArm, body, head, fourthLine, thirdLine, secondLine, firstLine];
 
   let word;
   let chosenWord;
@@ -14,7 +14,7 @@ window.onload = function () {
   let lives;
   let counter;
   let space;
-  let myStickman; // canvas
+  let stickman; // canvas
   let ctx; // canvas  ctx
   let letterButtons;
   let letters;
@@ -22,83 +22,83 @@ window.onload = function () {
   let wordHolder;
   let correct;
 
-  let showLives = document.getElementById("mylives");
+  let showLives = document.getElementById("lives");
   let showClue = document.getElementById("clue");
 
   
-// -------------------------- hangman draw ---------------------------
+// hangman
     // first line
-    function lowerFrame() {
-      drawOnCanvas(0, 200, 150, 200);
-  }
+   function firstLine() {
+    drawOnCanvas(0, 290, 150, 290);
+  };
   // second line
-  function verticalFrame() {
-      drawOnCanvas(10, 0, 10, 600);
-  }
+  function secondLine() {
+    drawOnCanvas(10, 0, 10, 600);
+  };
   // third line
-  function upperHorzontalFrame() {
-      drawOnCanvas(0, 5, 70, 5);
-  }
+  function thirdLine() {
+    drawOnCanvas(0, 10, 70, 10);
+  };
   //fourth line
-  function smallVerticalFrame() {
-      drawOnCanvas(60, 5, 60, 15);
-  }
-  // torso
-  function torso() {
-      drawOnCanvas(60, 36, 60, 70);
-  }
+  function fourthLine() {
+    drawOnCanvas(60, 10, 60, 30);
+  };
+  
   // head
   function head() {
-      myStickman = document.getElementById("canvas");
-      ctx = myStickman.getContext('2d');
-      ctx.beginPath();
-      ctx.strokeStyle = "red";
-      ctx.arc(60, 25, 10, 0, Math.PI * 2);
-      ctx.stroke();
-  }
-
+    stickman = document.getElementById("canvas");
+    ctx = stickman.getContext('2d');
+    ctx.beginPath();
+    ctx.strokeStyle = "white";
+    ctx.arc(60, 45, 15, 0, Math.PI * 2);
+    ctx.stroke();
+  };
+  // body
+  function body() {
+    drawOnCanvas(60, 36, 60, 70);
+  };
+  
   // right arm
   function rightArm() {
-      drawOnCanvas(60, 46, 100, 50);
-  }
-  
+    drawOnCanvas(60, 46, 100, 50);
+  };
   // left arm
   function leftArm() {
-      drawOnCanvas(60, 46, 20, 50);
-  }
+    drawOnCanvas(60, 46, 20, 50);
+  };
   // right leg
   function rightLeg() {
-      drawOnCanvas(60, 70, 100, 100);
-  }
+    drawOnCanvas(60, 70, 100, 100);
+  };
   // left leg
   function leftLeg() {
-      drawOnCanvas(60, 70, 20, 100);
-  }
+    drawOnCanvas(60, 70, 20, 100);
+  };
 
-  // Animate man
+// animate stickman
   function animate() {
-      var drawMe = lives;
-      drawArray[drawMe]();
-  }
+    var drawStickman = lives;
+    drawArray[drawStickman]();
+  };
 
+// canvas
   function canvas() {
-    myStickman = document.getElementById("canvas");
-    myStickman.width = 300;
-    myStickman.height = 200;
-    ctx = myStickman.getContext('2d');
-
-    //context.beginPath();
+    stickman = document.getElementById("canvas");
+    stickman.width = 300;
+    stickman.height = 300;
+  
+    ctx = stickman.getContext('2d');
     ctx.strokeStyle = "#000";
     ctx.lineWidth = 5;
-  } 
+  };
 
-function drawOnCanvas(pathFromX, pathFromY, pathToX, pathToY) {
+  function drawOnCanvas(pathFromX, pathFromY, pathToX, pathToY) {
     ctx.beginPath();
     ctx.strokeStyle = "whitesmoke";
     ctx.moveTo(pathFromX, pathFromY);
     ctx.lineTo(pathToX, pathToY);
     ctx.stroke();
-  }
+  };
 
   function buttons() {
     letterButtons = document.getElementById('buttons');
@@ -123,6 +123,7 @@ function drawOnCanvas(pathFromX, pathFromY, pathToX, pathToY) {
       correct.setAttribute('id', 'my-word');
       guess = document.createElement('li');
       guess.setAttribute('class', 'guess');
+      
       if (word[i] === "-") {
         guess.innerHTML = "-";
         space = 1;
@@ -135,11 +136,37 @@ function drawOnCanvas(pathFromX, pathFromY, pathToX, pathToY) {
       correct.appendChild(guess);
     };
   };
+
+  function hint() {
+    let hintIndex = wordsArr.indexOf(chosenWord);
+    showClue.innerHTML = "Definición: " +  hints[hintIndex];
+  };
   
- 
+  function check() {
+    list.onclick = function () {
+      let guess = (this.innerHTML);
+      this.setAttribute("class", "active");
+      this.onclick = null;
+      for (let i = 0; i < word.length; i++) {
+        if (word[i] === guess) {
+          guesses[i].innerHTML = guess;
+          counter++;
+        } 
+      };
+      let j = (word.indexOf(guess));
+      if (j === -1) {
+        lives--;
+        comments();
+        animate();
+      } else {
+        comments();
+      };
+    };
+  };
+
   function comments() {
     showLives.innerHTML = "Te quedan " + lives + " oportunidades";
-    if (lives < 1) {
+    if (lives < 1 || time === 10) {
       showLives.innerHTML = "¡Has perdido!";
     };
     for (let i = 0; i < guesses.length; i++) {
@@ -149,28 +176,6 @@ function drawOnCanvas(pathFromX, pathFromY, pathToX, pathToY) {
     };
   };
 
-  function check() {
-    list.onclick = function () {
-      let guess = (this.innerHTML);
-      this.setAttribute("class", "active");
-      this.onclick = null;
-      for (let i = 0; i < word.length; i++) {
-        if (word[i] === guess) {
-          guesses[i].innerHTML = guess;
-          counter += 1;
-        } 
-      };
-      let j = (word.indexOf(guess));
-      if (j === -1) {
-        lives -= 1;
-        comments();
-        animate();
-      } else {
-        comments();
-      };
-    };
-  };
-  
   function play() {
     chosenWord = wordsArr[Math.floor(Math.random() * wordsArr.length)];
     word = chosenWord.replace(/\s/g, "-");
@@ -181,16 +186,28 @@ function drawOnCanvas(pathFromX, pathFromY, pathToX, pathToY) {
     counter = 0;
     space = 0;
     answer();
-    comments();
     canvas();
+    comments();
+    hint();
+    startTime();
   }
   play();
 
-  function hint() {
-    let hintIndex = wordsArr.indexOf(chosenWord);
-    showClue.innerHTML = "Definición: " +  hints[hintIndex];
+  function startTime() {
+    let today = new Date();
+    let s = today.getSeconds();
+    // add a zero in front of numbers<10
+    s = checkTime(s);
+    document.getElementById("time").innerHTML = s + " segundos";
+    time = setTimeout(function(){ startTime() }, 15);
   };
-  hint();
+  
+  function checkTime(i) {
+    if (i<10) {
+      i = "0" + i;
+    }
+    return i;
+  };
 
   function resetGame() {
     correct.parentNode.removeChild(correct);
