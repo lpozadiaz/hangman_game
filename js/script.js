ea929b9c96192e5b30c9765dff4aab48b8f9ba99
Lorena Poza Díaz <lorenapozadiaz@MacLPD.local>
2019-09-05 19:16:24 +0200

    Some style

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ window.onload = function () {
         'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
   
-  let wordsArr = ["archiperres", "adefesio", "allende", "azafate", "cachivache", "correveidile", "cuchipanda", "entelequia", "floripondio", "lechuguino", "melifluo", "paparrucha", "pardiez", "picaflor", "plumier", "pocholo", "tarambana"];
-  let hints = ["trasto, cosa inútil", "persona o cosa ridícula, extravagante o muy fea", "más allá de", "canastillo, bandeja o fuente con borde de poca altura, tejidos de mimbres o hechos de paja, oro, plata, latón, loza u otras materias", "cosa rota o inservible", "persona que lleva y trae cuentos y chismes","comida que toman juntas y regocijadamente varias personas", "cosa irreal", "flor grande que suele figurar en adornos de mal gusto", "muchacho imberbe que se mete a galantear aparentando ser hombre hecho", "que tiene miel o es parecido a ella en sus propiedades", "noticia falsa y desatinada de un suceso, esparcida entre el vulgo", "par Dios", "frívolo inconstante", "caja o estuche que sirve para guardar plumas, lápices, etc", "bonito, atractivo o agradable", "persona alocada, de poco juicio"];
+  let wordsArr = ["archiperres", "ardora", "adefesio", "allende", "azafate", "cachivache", "correveidile", "cuchipanda", "entelequia", "floripondio", "lechuguino", "melifluo", "paparrucha", "pardiez", "picaflor", "plumier", "pocholo", "tarambana"];
+  let hints = ["trasto, cosa inútil", "fosforescencia del mar que indica la presencia de un banco de sardinas", "persona o cosa ridícula, extravagante o muy fea", "más allá de", "canastillo, bandeja o fuente con borde de poca altura, tejidos de mimbres o hechos de paja, oro, plata, latón, loza u otras materias", "cosa rota o inservible", "persona que lleva y trae cuentos y chismes","comida que toman juntas y regocijadamente varias personas", "cosa irreal", "flor grande que suele figurar en adornos de mal gusto", "muchacho imberbe que se mete a galantear aparentando ser hombre hecho", "que tiene miel o es parecido a ella en sus propiedades", "noticia falsa y desatinada de un suceso, esparcida entre el vulgo", "par Dios", "frívolo inconstante", "caja o estuche que sirve para guardar plumas, lápices, etc", "bonito, atractivo o agradable", "persona alocada, de poco juicio"];
   let drawArray = [rightLeg, leftLeg, rightArm, leftArm, body, head, fourthLine, thirdLine, secondLine, firstLine];
 
   let word;
@@ -36,55 +36,56 @@ window.onload = function () {
   let showClue = document.getElementById("clue");
   let showClock = document.getElementById("time");
   let showButton = document.getElementById("reset");
+  let finalMessage = document.getElementById("message");
 
 // hangman
     // first line
    function firstLine() {
-    drawOnCanvas(0, 290, 150, 290);
+    drawOnCanvas(30, 270, 270, 270);
   };
   // second line
   function secondLine() {
-    drawOnCanvas(10, 0, 10, 600);
+    drawOnCanvas(30, 30, 30, 270);
   };
   // third line
   function thirdLine() {
-    drawOnCanvas(0, 10, 70, 10);
+    drawOnCanvas(30, 30, 150, 30);
   };
   //fourth line
   function fourthLine() {
-    drawOnCanvas(60, 10, 60, 30);
+    drawOnCanvas(150, 30, 150, 70);
   };
   // head
   function head() {
     ctx.beginPath();
     ctx.strokeStyle = "#1f3a93";
-    ctx.arc(60, 45, 15, 0, Math.PI * 2);
+    ctx.arc(150, 85, 15, 0, Math.PI * 2);
     ctx.stroke();
     ctx.closePath();
   };
   // body
   function body() {
-    drawOnCanvas(60, 60, 60, 100);
+    drawOnCanvas(150, 100, 150, 150);
   };
 
   // left arm
    function leftArm() {
-    drawOnCanvas(20, 60, 60, 60);
+    drawOnCanvas(150, 100, 180, 100);
   };
 
   // right arm
   function rightArm() {
-    drawOnCanvas(60, 60, 100, 60);
+    drawOnCanvas(120, 100, 150, 100);
   };
  
   // left leg
   function leftLeg() {
-    drawOnCanvas(40, 120, 60, 100);
+    drawOnCanvas(150, 150, 180, 180);
   };
 
   // right leg
   function rightLeg() {
-    drawOnCanvas(60, 100, 80, 120);
+    drawOnCanvas(120, 180, 150, 150);
   };
 
 // animate stickman
@@ -97,7 +98,7 @@ window.onload = function () {
   function canvas() {
     ctx.strokeStyle = "#1f3a93";
     ctx.lineCap = "round";
-    ctx.lineWidth = 5;
+    ctx.lineWidth = 2.5;
   };
 
   function drawOnCanvas(pathFromX, pathFromY, pathToX, pathToY) {
@@ -128,7 +129,7 @@ window.onload = function () {
         ctx.stroke();
         ctx.closePath();
       }
-    }, 10);
+    }, 1);
   };
 
 // Alphabet buttons
@@ -157,7 +158,6 @@ window.onload = function () {
     for (let i = 0; i < word.length; i++) {
       letterSpaces.setAttribute('id', 'my-word');
       guess = document.createElement('li');
-      guess.setAttribute('class', 'guess');
       
       if (word[i] === "-") {
         guess.innerHTML = "-";
@@ -184,7 +184,6 @@ window.onload = function () {
   function check() {
     list.onclick = function () {
       let guess = (this.innerHTML);
-      this.setAttribute("class", "active");
       this.onclick = null;
       for (let i = 0; i < word.length; i++) {
         if (word[i] === guess) {
@@ -211,6 +210,7 @@ window.onload = function () {
 // Play game
 
   function play() {
+    finalMessage.innerHTML = "";
     chosenWord = wordsArr[Math.floor(Math.random() * wordsArr.length)];
     word = chosenWord.replace(/\s/g, "-");
     buttons();
@@ -234,11 +234,13 @@ window.onload = function () {
     time = setTimeout(timedCount, 1000);
 
     if (lives < 1 || count === -1) {
-      showLives.innerHTML = "¡Has perdido!";
+      finalMessage.innerHTML = "¡Has perdido!";
+      letterSpaces.setAttribute('id', 'answer');
+      letterSpaces.innerHTML = "La respuesta correcta es: " + chosenWord.toUpperCase()
       clearTimeout(time);
       stopGame();
     } else if (counter + space === guesses.length) {
-        showLives.innerHTML = "¡Muy listo!";
+        finalMessage.innerHTML = "¡Muy listo!";
         clearTimeout(time);
         stopGame();
       };
@@ -266,10 +268,9 @@ window.onload = function () {
     letters.parentNode.removeChild(letters);
     showClock.innerHTML = "";
     showClue.innerHTML = "";
-    letterSpaces.innerHTML = "La respuesta correcta es: " + chosenWord.toUpperCase();
+    showLives.innerHTML = "";
     clearInterval(intervalID);
     ctx.clearRect(0, 0, w, h); 
-    
     startButton();
   };
 
